6279da841e0eb689e1eb0004a5fb2ccfe009f9d9
ChiAnh2409 <doanvan2409@gmail.com>
2023-12-06 22:10:51 +0700

    first commit

---
+++ QLPG/QLPG/Controllers/AdminController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLPG.Models;
+using QLPG.ViewModel;
+
+namespace QLPG.Controllers
+{
+    public class AdminController : Controller
+    {
+        private QLPG1Entities db = new QLPG1Entities();
+
+        public ActionResult Index()
+        {
+            MultipleData data = new MultipleData();
+
+            // Lấy toàn bộ dữ liệu
+            data.hoiViens = db.HoiVien.Include("ThanhVien").ToList();
+            data.vien = db.ThanhVien.ToList();
+            data.chiTietDK_ = db.ChiTietDK_GoiTap.ToList();
+            // Lấy số thông báo mới từ TempData
+            int newNotificationCount = (int)(TempData["NewNotificationCount"] ?? 0);
+
+            // Truyền số thông báo mới cho view
+            ViewBag.NewNotificationCount = newNotificationCount;
+
+            // Lấy thông báo đăng ký từ TempData hoặc Session
+            string registrationNotification = "";
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var currentUser = db.Account.SingleOrDefault(u => u.Username == User.Identity.Name);
+
+                if (currentUser != null)
+                {
+                    registrationNotification = (string)(Session[$"{currentUser.Username}_RegistrationNotification"] ?? "");
+                    Session[$"{currentUser.Username}_RegistrationNotification"] = null; // Đánh dấu là đã đọc
+                }
+            }
+            else
+            {
+                registrationNotification = (string)TempData["RegistrationNotification"];
+            }
+
+            // Truyền thông báo đăng ký cho view
+            ViewBag.RegistrationNotification = registrationNotification;
+
+            // Tính số lượng hội viên mới
+            DateTime today = DateTime.Now.Date;
+            int newMembersCount = data.hoiViens.Count(hv => hv.NgayGiaNhap.HasValue && hv.NgayGiaNhap.Value.Date == today);
+
+            // Truyền số lượng hội viên mới cho view
+            ViewBag.NewMembersCount = newMembersCount;
+
+            return View(data);
+        }
+
+        // Trong phương thức xử lý thông báo đăng ký tập thử
+        [HttpPost]
+        public ActionResult Register(string tenTV)
+        {
+            // Xử lý đăng ký
+
+            // Tạo thông báo đăng ký và lưu vào TempData
+            string registrationNotification = $"Đăng ký thành công cho {tenTV} vào ngày {DateTime.Now}.";
+            TempData["AdminNotification"] = registrationNotification;
+
+            // Lấy số thông báo mới từ TempData
+            int newNotificationCount = (int)(TempData["NewNotificationCount"] ?? 0);
+
+            // Truyền số thông báo mới và thông báo đăng ký cho view
+            var data = new MultipleData
+            {
+                NewNotificationCount = newNotificationCount + 1,
+                RegistrationNotification = registrationNotification
+            };
+
+            // Redirect hoặc trả về view tương ứng
+            return RedirectToAction("Index", data); // hoặc trả về view tương ứng
+        }
+        [HttpGet]
+        public JsonResult DoanhThu()
+        {
+            var DT = db.ChiTietDK_GoiTap
+                .Where(ct => ct.NgayBatDau.HasValue && ct.NgayBatDau.Value.Year == 2023 && ct.ThanhTien.HasValue)
+                .GroupBy(ct => new { Thang = ct.NgayBatDau.Value.Month, Nam = ct.NgayBatDau.Value.Year })
+                .Select(g => new { Thang = g.Key.Thang, Nam = g.Key.Nam, TongDoanhThu = g.Sum(ct => ct.ThanhTien) })
+                .OrderBy(g => g.Nam).ThenBy(g => g.Thang)
+                .ToList();
+
+            return Json(DT, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ QLPG/QLPG/Controllers/HoiVienController.cs
@@ -0,0 +1,261 @@
+﻿using QLPG.Models;
+using QLPG.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace QLPG.Controllers
+{
+    public class HoiVienController : Controller
+    {
+        private QLPG1Entities db = new QLPG1Entities();
+        //tạo biến database để lấy dữ liệu
+        // GET: HoiVien
+        public ActionResult HoiVien()
+        {
+            var list = new MultipleData();
+            list.hoiViens = db.HoiVien.Include("ThanhVien").ToList();
+            //tình trạng gói tập của hội viên
+            foreach (var hoiVien in list.hoiViens)
+            {
+                var goiTap = db.ChiTietDK_GoiTap
+                                .Where(ct => ct.id_HV == hoiVien.id_HV)
+                                .OrderByDescending(ct => ct.NgayKetThuc)
+                                .FirstOrDefault();
+
+                if (goiTap != null && goiTap.NgayKetThuc >= DateTime.Now)
+                {
+                    hoiVien.TinhTrang = true;
+                }
+                else
+                {
+                    hoiVien.TinhTrang = false;
+                }
+
+                // Cập nhật trạng thái của hội viên trong cơ sở dữ liệu
+                db.Entry(hoiVien).State = EntityState.Modified;
+            }
+
+            // Lưu các thay đổi vào cơ sở dữ liệu
+            db.SaveChanges();
+
+            list.vien = db.ThanhVien.ToList();
+            return View(list);
+        }
+        public ActionResult ThemHV() 
+        {
+            // Lấy danh sách thành viên chưa là hội viên
+            var chuaLaHoiVienIds = db.HoiVien.Select(hv => hv.id_TV).ToList();
+            var chuaLaHoiVien = db.ThanhVien.Where(tv => !chuaLaHoiVienIds.Contains(tv.id_TV)).ToList();
+
+            var list = new MultipleData
+            {
+                hoiViens = db.HoiVien.Include("ThanhVien"),
+                vien = chuaLaHoiVien
+            };
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult ThemHV(HoiVien hv)
+        {
+            String HinhAnh = "";
+
+            HttpPostedFileBase file = Request.Files["HinhAnh"];
+            if (file != null && file.FileName != "")
+            {
+                String serverPath = HttpContext.Server.MapPath("~/assets/img/team");
+                String filePath = serverPath + "/" + file.FileName;
+                file.SaveAs(filePath);
+                HinhAnh = file.FileName;
+            }
+            hv.HinhAnh = HinhAnh;
+            db.HoiVien.Add(hv);
+            hv.TinhTrang = true;
+            DateTime now = DateTime.Now;
+            hv.NgayGiaNhap = now;
+            db.SaveChanges();
+            return RedirectToAction("HoiVien");
+        }
+        public ActionResult SuaHV(int id)
+        {
+            // Lấy thông tin hội viên hiện tại từ CSDL
+            var existingHoiVien = db.HoiVien.Find(id);
+
+            // Lấy danh sách thành viên chưa là hội viên
+            var chuaLaHoiVienIds = db.HoiVien.Select(hv => hv.id_TV).ToList();
+            var chuaLaHoiVien = db.ThanhVien.Where(tv => !chuaLaHoiVienIds.Contains(tv.id_TV)).ToList();
+
+            var viewmodel = new MultipleData
+            {
+                hoiViens = existingHoiVien != null ? new List<HoiVien> { existingHoiVien } : new List<HoiVien>(),
+                vien = chuaLaHoiVien
+            };
+
+            return View(viewmodel);
+        }
+        [HttpPost]
+        public ActionResult SuaHV(HoiVien hv)
+        {
+            HoiVien existingHoiVien = db.HoiVien.Find(hv.id_HV);
+            if (existingHoiVien != null)
+            {
+                existingHoiVien.id_TV = hv.id_TV;
+                existingHoiVien.NgaySinh = hv.NgaySinh;
+                existingHoiVien.CCCD = hv.CCCD;
+                existingHoiVien.TinhTrang = hv.TinhTrang;
+
+                // Kiểm tra và lưu hình ảnh nếu có
+                HttpPostedFileBase file = Request.Files["HinhAnh"];
+                if (file != null && file.FileName != "")
+                {
+                    String HinhAnh = file.FileName;
+                    String serverPath = HttpContext.Server.MapPath("~/assets/img/team");
+                    String filePath = serverPath + "/" + HinhAnh;
+                    file.SaveAs(filePath);
+                    existingHoiVien.HinhAnh = HinhAnh;
+                }
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("HoiVien");
+        }
+        public ActionResult XoaHV(int id)
+        {
+            var HoiVien = db.HoiVien.Find(id);
+            if (HoiVien != null)
+            {
+                db.HoiVien.Remove(HoiVien);
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("HoiVien");
+        }
+        [HttpPost] //Tìm kiếm bằng tên trong bảng hội viên nhưng tham chiếu bằng id_TV trong bảng thành viên
+        public ActionResult TimKiemHV(string search)
+        {
+            var list = new MultipleData();
+            list.hoiViens = db.HoiVien.Include("ThanhVien").Where(hv => hv.ThanhVien.TenTV.Contains(search)).ToList();
+            list.vien = db.ThanhVien.ToList();
+
+            return View("HoiVien", list);
+        }
+
+        // Điểm danh cho hội viên
+        [HttpGet]
+        public ActionResult DiemDanhHV(int id_HV)
+        {
+            var hoiVien = db.HoiVien.Include("ThanhVien").FirstOrDefault(hv => hv.id_HV == id_HV);
+
+            if (hoiVien != null)
+            {
+                return View(hoiVien);
+            }
+            // Kiểm tra xem có thông báo điểm danh thành công không
+            if (TempData["DiemDanhSuccess"] != null)
+            {
+                ViewBag.DiemDanhSuccess = TempData["DiemDanhSuccess"].ToString();
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost]
+        public ActionResult DiemDanhHV(int id_HV, bool DaDiemDanh)
+        {
+            var hoiVien = db.HoiVien.Find(id_HV);
+            if (hoiVien != null)
+            {
+                // Kiểm tra ModelState.IsValid
+                if (ModelState.IsValid)
+                {
+                    // Lấy ngày hiện tại
+                    var currentDate = DateTime.Now.Date;
+
+                    // Tạo biến tạm để lưu ngày kết thúc của ngày hiện tại
+                    var nextDate = currentDate.AddDays(1);
+
+                    // Lấy buổi tập của hội viên trong ngày
+                    var buoiTapTrongNgay = db.BuoiTap
+                        .Where(bt => bt.id_HV == id_HV && bt.NgayThamGia.HasValue && bt.NgayThamGia.Value >= currentDate && bt.NgayThamGia.Value < nextDate)
+                        .FirstOrDefault();
+
+                    if (buoiTapTrongNgay == null)
+                    {
+                        // Hội viên chưa điểm danh trong ngày, thêm mới buổi tập
+                        var buoiTap = new BuoiTap
+                        {
+                            id_HV = id_HV,
+                            DaDiemDanh = DaDiemDanh,
+                            NgayThamGia = currentDate
+                        };
+
+                        db.BuoiTap.Add(buoiTap);
+                        db.SaveChanges();
+
+                        // Thông báo đã điểm danh thành công
+                        TempData["DiemDanhSuccess"] = "Đã điểm danh thành công.";
+
+                        // Chuyển hướng về trang DiemDanhHV với tham số id_HV
+                        return RedirectToAction("DiemDanhHV", new { id_HV = id_HV });
+                    }
+                    else
+                    {
+                        // Thông báo rằng hội viên đã điểm danh trong ngày
+                        ModelState.AddModelError("", "Hội viên đã điểm danh trong ngày.");
+                    }
+                }
+            }
+
+            return HttpNotFound();
+        }
+
+        // Xem chi tiết điểm danh
+        public ActionResult CTDiemDanh(int id_HV, DateTime ngay)
+        {
+            var list = new MultipleData();
+            list.hoiViens = db.HoiVien.Include("ThanhVien").ToList();
+            list.vien = db.ThanhVien.ToList();
+
+            // Lấy thông tin hội viên
+            var hoiVien = db.HoiVien.Include("ThanhVien").FirstOrDefault(hv => hv.id_HV == id_HV);
+
+            // Kiểm tra xem hội viên đã điểm danh trong ngày chưa
+            var daDiemDanhTrongNgay = db.BuoiTap
+                .Any(bt => bt.id_HV == id_HV && bt.NgayThamGia.HasValue &&
+                           bt.NgayThamGia.Value.Year == ngay.Year &&
+                           bt.NgayThamGia.Value.Month == ngay.Month &&
+                           bt.NgayThamGia.Value.Day == ngay.Day);
+
+            if (daDiemDanhTrongNgay)
+            {
+                // Lấy danh sách buổi tập của hội viên trong ngày cụ thể
+                var buoiTaps = db.BuoiTap
+                    .Where(bt => bt.id_HV == id_HV && bt.NgayThamGia.HasValue &&
+                           bt.NgayThamGia.Value.Year == ngay.Year &&
+                           bt.NgayThamGia.Value.Month == ngay.Month &&
+                           bt.NgayThamGia.Value.Day == ngay.Day)
+                    .ToList();
+
+                // Lọc danh sách buổi tập theo thời gian và chỉ giữ lại một bản ghi cho mỗi thời gian
+                var distinctBuoiTaps = buoiTaps
+                    .GroupBy(bt => bt.NgayThamGia)
+                    .Select(group => group.First())
+                    .ToList();
+
+                ViewBag.TenHoiVien = hoiVien.ThanhVien.TenTV;  // Truyền tên hội viên vào ViewBag
+                list.buoiTap = distinctBuoiTaps;
+            }
+            else
+            {
+                ViewBag.TenHoiVien = hoiVien.ThanhVien.TenTV;
+                list.buoiTap = new List<BuoiTap>(); // Không có buổi tập nào nếu chưa điểm danh
+            }
+
+            return View(list);
+        }
+    }
+}
